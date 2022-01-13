9370d55d091bb72d919a20cb69e16411fbb50e1d
Maksym Dybarskyi <maksym.dybarskyi@gymondo.de>
2022-01-13 19:55:00 +0100

    Upgrade kotlin and libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	val kotlinVersion = "1.4.21"
+	val kotlinVersion = "1.6.10"
 	kotlin("jvm") version kotlinVersion
 	kotlin("kapt") version kotlinVersion
 	kotlin("plugin.serialization") version kotlinVersion
@@ -19,10 +19,10 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 
-	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
-	implementation("com.squareup.okhttp3:okhttp:4.9.0")
+	implementation("com.squareup.okhttp3:okhttp:4.9.3")
 	implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 }
