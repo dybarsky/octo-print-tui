074413c1f0d4b7face15fd5ef2fca2283dfa8224
Maksym Dybarskyi <maxim.dybarskyy@gmail.com>
2023-11-24 13:12:04 +0100

    Upgrade kotlin and libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
-	val kotlinVersion = "1.6.10"
+	val kotlinVersion = "1.9.20"
 	kotlin("jvm") version kotlinVersion
-	kotlin("kapt") version kotlinVersion
 	kotlin("plugin.serialization") version kotlinVersion
+	id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 	application
 }
 
 application {
-	mainClassName = "App"
+	mainClass.set("App")
 }
 
 repositories {
-	jcenter()
 	mavenCentral()
 	maven("https://kotlin.bintray.com/kotlinx")
 }
@@ -19,10 +18,10 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib"))
 
-	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+	implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
-	implementation("com.squareup.okhttp3:okhttp:4.9.3")
-	implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+	implementation("com.squareup.okhttp3:okhttp:4.12.0")
+	implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 }
